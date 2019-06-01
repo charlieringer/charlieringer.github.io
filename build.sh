88a69a1321a99b51dd8d568bd657eadfb29dab36
Charlie <charlie@Dinosaurus.local>
2019-06-01 08:00:18 +0100

    Working on having previews for blogs on the main landing page. All seems to be working ok. Next job is making them appear in the right order. No easy feat!

--- build.sh
+++ build.sh
@@ -10,8 +10,10 @@ shopt -s nullglob
 suffix=".md"
 prefix="src/blogs/"
 
-cat src/header.html src/blog.html > blog.html
+# Start buiilding the landing page
+cat src/header.html src/blog_landing.html > blog.html
 
+#Go through each blog
 for f in src/blogs/*.md
 do
 	#Get the file name
@@ -21,13 +23,22 @@ do
 	#Get the title (for index building purposes)
 	title=`grep -m 1 "^# .*" $f | sed s/"# "//g`
 	date=${filename:0:10}
+	
 	#Turn the markdown into html
-	pandoc $f > src/blog_html/$filename.html
-	#And save it to as the finished file
-	cat src/header.html src/blog_html/$filename.html src/blog_footer.html > $finaldest
+	pandoc $f > src/tmp.html
 
-	#And add it to the blog page
-	cat src/blog_template.html | sed "s#{{FILE}}#$finaldest#g" | sed "s#{{TITLE}}#$title#g" | sed "s#{{DATE}}#$date#g" |  sed "s#{{SAMPLE_BODY}}#$finaldest#g" >> blog.html
+	#And save it to as the finished file
+	cat src/header.html src/tmp.html src/blog_footer.html > $finaldest
+
+	#Get some sample content (400 chars)
+	sample=`grep -o '<p>.*</p>' src/tmp.html` 
+	sample=${sample:0:400}
+
+	#Remove temp file
+	rm src/tmp.html
+
+	#And add a short linked version to the blog landing page
+	cat src/blog_short.html | sed "s#{{FILE}}#$finaldest#g" | sed "s#{{TITLE}}#$title#g" | sed "s#{{DATE}}#$date#g" |  sed "s#{{SAMPLE_BODY}}#$sample#g" >> blog.html
 
 done
 
