67cd97499194be02ae453b3c59f1805f85de374b
Charlie <charlie@dinosaurus.wkstn.qmul.ac.uk>
2019-08-21 23:23:13 +0100

    Adding more files (slides etc) to publicatikns

---
+++ src/blogs/test.py
@@ -0,0 +1,11 @@
+import keras
+import tensorflow
+import numpy as np
+import pandas
+
+print("Keras version {}".format(keras.__version__))
+print("TF version {}".format(tensorflow.__version__))
+print("Numpy version {}".format(np.__version__))
+print("Pandas version {}".format(pandas.__version__))
+
+print("Everything imported ok")
